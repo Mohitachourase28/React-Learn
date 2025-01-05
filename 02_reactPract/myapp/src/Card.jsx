fc76e5e27698b0929a37b3d1d673074f082f49c6
Mohita Chourase <mohitachourase03@gmail.com>
2025-01-05 19:56:04 +0530

    Add new folder 02_reactPract

---
+++ 02_reactPract/myapp/src/Card.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
+function Card() {
+  return(
+    <div className="card">
+        <img className="card-image" src="https://via.placeholder.com/200" alt="profile picture" />
+        <h2 className="card-title">Mohita Chourase</h2>
+        <p className="card-text">I'm studying react!</p>
+    </div>
+  );
+}
+
+export default Card;
