5bb2f5c48dcb266a3ebcc5c8a3ef402d733d0659
Mohita Chourase <mohitachourase03@gmail.com>
2025-01-11 22:40:24 +0530

    Add new folder: 18_reactPract

---
+++ 18_reactPract/myapp/src/ComponentD.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+/**Prop drilling */
+import React, {useContext} from 'react';
+import { UserContext } from './ComponentA';
+
+function ComponentD(){
+
+    const user = useContext(UserContext)
+    return(
+        <div className="box">
+            <h1>ComponentD</h1>
+            <h2>{`Bye ${user}`}</h2>
+        </div>
+    );
+}
+export default ComponentD;
